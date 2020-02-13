119deffede498392054f8e4510518c98d9ac268d
Nikhil <43577457+KnifeParty12@users.noreply.github.com>
2020-02-13 21:24:19 +0530

    Added missing API for create and delete for user-status

--- app/api/server/v1/custom-user-status.js
+++ app/api/server/v1/custom-user-status.js
@@ -1,18 +1,47 @@
+import { Meteor } from 'meteor/meteor';
+import { Match, check } from 'meteor/check';
+
+import { CustomUserStatus } from '../../../models';
 import { API } from '../api';
 import { findCustomUserStatus } from '../lib/custom-user-status';
 
-API.v1.addRoute('custom-user-status.list', { authRequired: true }, {
-	get() {
-		const { offset, count } = this.getPaginationItems();
-		const { sort, query } = this.parseJsonQuery();
-
-		return API.v1.success(Promise.await(findCustomUserStatus({
-			query,
-			pagination: {
-				offset,
-				count,
-				sort,
-			},
+@@ -16,3 +20,69 @@ API.v1.addRoute('custom-user-status.list', { authRequired: true }, {
 		})));
 	},
 });
+
+API.v1.addRoute('custom-user-status.create', { authRequired: true }, {
+	post() {
+		check(this.bodyParams, {
+			name: String,
+			statusType: Match.Maybe(String),
+		});
+
+		const userStatusData = {
+			name: this.bodyParams.name,
+			statusType: this.bodyParams.statusType,
+		};
+
+		Meteor.runAsUser(this.userId, () => {
+			Meteor.call('insertOrUpdateUserStatus', userStatusData);
+		});
+
+		return API.v1.success({
+			customUserStatus: CustomUserStatus.findOneByName(userStatusData.name),
+		});
+	},
+});
+
+API.v1.addRoute('custom-user-status.delete', { authRequired: true }, {
+	post() {
+		const { customUserStatusId } = this.bodyParams;
+		if (!customUserStatusId) {
+			return API.v1.failure('The "customUserStatusId" params is required!');
+		}
+
+		Meteor.runAsUser(this.userId, () => Meteor.call('deleteCustomUserStatus', customUserStatusId));
+
+		return API.v1.success();
+	},
+});
+
